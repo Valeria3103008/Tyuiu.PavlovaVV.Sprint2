ce9476554190bf793edd8d7a6ee693869c7fa780
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-10-12 18:43:43 +0500

    Task5

---
+++ Tyuiu.PavlovaVV.Sprint2.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.PavlovaVV.Sprint2.Task5.V9.Lib;
+namespace Tyuiu.PavlovaVV.Sprint2.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindMonthName()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("Январь", ds.FindDateOfNextDay(1));
+            Assert.AreEqual("Февраль", ds.FindDateOfNextDay(2));
+            Assert.AreEqual("Март", ds.FindDateOfNextDay(3));
+            Assert.AreEqual("Апрель", ds.FindDateOfNextDay(4));
+            Assert.AreEqual("Май", ds.FindDateOfNextDay(5));
+            Assert.AreEqual("Июнь", ds.FindDateOfNextDay(6));
+            Assert.AreEqual("Июль", ds.FindDateOfNextDay(7));
+            Assert.AreEqual("Август", ds.FindDateOfNextDay(8));
+            Assert.AreEqual("Сентябрь", ds.FindDateOfNextDay(9));
+            Assert.AreEqual("Октябрь", ds.FindDateOfNextDay(10));
+            Assert.AreEqual("Ноябрь", ds.FindDateOfNextDay(11));
+            Assert.AreEqual("Декабрь", ds.FindDateOfNextDay(12));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDateOfNextDay(-1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindDateOfNextDay(13);
+            });
+
+
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint2.Task5.V9/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.PavlovaVV.Sprint2.Task5.V9.Lib;
+namespace Tyuiu.PavlovaVV.Sprint2.Task5.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+            Console.WriteLine("Введите номер месяца: ");
+            int numMounth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите номер дня: ");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+
+            if ((((numMounth < 1) || (numMounth > 12)) && ((numDay < 1) || (numDay > 30))) && ((numMounth == 4) || (numMounth == 9) || (numMounth == 6) || (numMounth == 11)))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = ds.FindDateOfNextDay(numMounth);
+            }
+
+            if ((((numMounth < 1) || (numMounth > 12)) && ((numDay < 1) || (numDay > 31))) && ((numMounth == 1) || (numMounth == 2) || (numMounth == 5) || (numMounth == 11) || (numMounth == 7) || (numMounth == 8) || (numMounth == 10) || (numMounth == 12)))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = ds.FindDateOfNextDay(numMounth);
+            }
+
+            if ((((numMounth < 1) || (numMounth > 12)) && ((numDay < 1) || (numDay > 28))) && (numMounth == 2))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = ds.FindDateOfNextDay(numMounth);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Дата следующего дня= Месяц: " + ds.FindDateOfNextDay(numMounth) + " День: " + (numDay + 1));
+            Console.ReadKey();
+
+        }
+    }
+}
