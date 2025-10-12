a5b456380e200b77332327b2e2531934ca8443c8
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-10-12 18:16:33 +0500

    Task4

---
+++ Tyuiu.PavlovaVV.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.PavlovaVV.Sprint2.Task4.V15.Lib;
+namespace Tyuiu.PavlovaVV.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 441;
+            double res = ds.Calculate(x, y);
+            double wait = 12.384;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint2.Task4.V15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Tyuiu.PavlovaVV.Sprint2.Task4.V15.Lib;
+namespace Tyuiu.PavlovaVV.Sprint2.Task4.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            double res = ds.Calculate(x, y);
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Значение переменной = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
