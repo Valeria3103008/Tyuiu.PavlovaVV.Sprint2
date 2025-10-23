06057ab36475f91d43f128d334b535de382fb788
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-10-24 00:24:15 +0500

    Task6

---
+++ Tyuiu.PavlovaVV.Sprint2.Task6.V15.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.PavlovaVV.Sprint2.Task6.V15.Lib
+{
+    public class DataService : ISprint2Task6V15
+    {
+        public string FindDayName(int k)
+        {
+            int n;
+            string res = "";
+            n = k % 7 + d - 1;
+            if (n > 7)
+                n = n - 7;
+
+            switch (n)
+            {
+                case 1: return "Понедельник";
+                case 2: return "Вторник";
+                case 3: return "Среда";
+                case 4: return "Четверг";
+                case 5: return "Пятница";
+                case 6: return "Суббота";
+                case 7: return "Воскресение";
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.PavlovaVV.Sprint2.Task6.V15/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.PavlovaVV.Sprint2.Task6.V15.Lib;
+namespace Tyuiu.PavlovaVV.Sprint2.Task6.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                DataService ds = new DataService();
+                int k, d, n;
+                do
+                {
+                    Console.WriteLine("Введите номер дня не високосного года ");
+                    k = Convert.ToInt32(Console.ReadLine());
+                } while (k <= 0 || k >= 365);
+
+                do
+                {
+                    Console.Write("Введите номер дня недели 1 января d = ");
+                    d = Convert.ToInt32(Console.ReadLine());
+                } while (d < 1 || d > 7);
+
+                Console.WriteLine(ds.FindDayName(k, d));
+                Console.ReadKey();
+            }
+    }
+}
