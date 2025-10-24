bf555a772b850f764894536ffa38c2eaad04da7d
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-10-24 16:41:12 +0500

    Task7

---
+++ Tyuiu.PavlovaVV.Sprint2.Task7.V3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.PavlovaVV.Sprint2.Task7.V3.Lib;
+
+namespace Tyuiu.PavlovaVV.Sprint2.Task7.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res)
+            {
+                Console.WriteLine("Верно");
+            }
+            else
+            {
+                Console.WriteLine("Неверно");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
