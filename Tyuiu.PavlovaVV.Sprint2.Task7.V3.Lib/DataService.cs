d11a17c9a2960e957a305b356270b150e17a05f6
Валерия Павлова <lerochka_pavlova.08@mail.ru>
2025-11-01 10:15:15 +0500

    Task7

--- Tyuiu.PavlovaVV.Sprint2.Task7.V3.Lib/DataService.cs
+++ Tyuiu.PavlovaVV.Sprint2.Task7.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.PavlovaVV.Sprint2.Task7.V3.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y - 1, 2) <= 1) && (x > 0 && (x <= 1) && ((y > 0)) && (x <= 1)))
+            if (Math.Pow(x, 2) + Math.Pow((y - 1), 2) <= 1 && y <= 1 - Math.Pow(x, 2))
             {
                 res = true;
             }
